dc5f511ce32004f7ff3d70cfb33f3bfa7f5bb772
Barış Bütün <baris.butun15@gmail.com>
2025-02-08 19:31:21 +0300

    Created Project
    new project

---
+++ VoiceSpringBoot/src/main/java/org/example/voicespringboot/VoiceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.voicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceSpringBootApplication.class, args);
+    }
+
+}
---
+++ VoiceSpringBoot/src/test/java/org/example/voicespringboot/VoiceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.voicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
